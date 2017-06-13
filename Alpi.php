072ef1b7dea973bd7092fa04d2bfaa3a80cda392
Jacopo Fioretti <jpfiore13@gmail.com>
2017-06-13 23:27:53 +0200

    Add files via upload

---
+++ Alpi.php
@@ -0,0 +1,25 @@
+<?php
+
+	$title = "Alpi.php";
+
+	include_once 'header.php';
+
+?>
+				<h1>Le Alpi </h1>
+				</header>
+					<p>Le Alpi sono la catena montuosa piu' importante d'Europa. Si chiamano in francese Alpes, in tedesco Alpen,
+						in sloveno Alpe. Sesto Pompeo Festo nel suo Primo Libro attesta che il nome deriva da ALBUS (bianco) che
+						i Sabini pronunciavano Alpus e indicava il colore sempre bianco della catena innevata anche durante la
+						stagione estiva.</P>
+						<br>
+					<p>Per convenzione le Alpi iniziano a ovest del Colle di Cadibona, anche se in realta' il confine geologico e'
+						posto nei pressi di Genova ed e' costituito dal Gruppo di Voltri, terminano a ovest della citta'di <a href="http://www.vienna.it"/> Vienna</a>
+						coprendo una distanza di circa 1.300 km a forma di arco tra l'Italia Settentrionale, la Francia sud-orientale,
+						la Svizzera, il Liechtenstein, l'Austria, il sud della Germania, la Slovenia e l'Ungheria occidentale. Tra
+						Verona e Monaco di Baviera raggiungono la larghezza massima.</P>
+					<img src="images/alpi.jpg"/> <br>Le Alpi viste dallo spazio, maggio 2002</a>
+<?php
+
+	include_once 'footer.php';
+
+?>
---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+	$title = "index.php";
+
+	include_once 'header.php';
+
+?>
+				<h1>Piemonte</h1>
+			</header>
+					<p>Il <strong>Piemonte</strong> e' una regione dell'Italia nord-occidentale di 4.377.791 abitanti con capoluogo Torino. Confina
+							ad ovest con la Francia, a nord-ovest con la Valle d'Aosta, a nord con la Svizzera, ad est con la Lombardia,
+							a sud-est con l'Emilia-Romagna e a sud con la Liguria.</P>
+							<br>
+					<p>Il Piemonte e' la seconda regione italiana per superficie, dopo la Sicilia, e' la sesta per numero di abitanti. E'
+						inoltre la quarta regione italiana per esportazioni, con una quota del 10% sul totale nazionale.</p>
+					<img src="images/mappa-piemonte.jpg"/><br>Il Piemonte e le sue province</a>
+				<h2>Geografia</h2>
+					<p>Il territorio della regione e' suddivisibile in tre fasce concentriche, di cui la prevalente e piu' esterna e' quella
+						alpina ed appenninica. Al suo interno vi e'la zona collinare, la quale racchiude la zona pianeggiante.</p>
+						<br>
+				<p>Nella regione scorrono moltissimi fiumi e torrenti, tutti affluenti del fiume Po che nasce al Pian del Re ai
+					piedi del Monviso.</p>
+				<h2> Monti</h2>
+					<p>Le principali catene montuose sono le <a href="Alpi.html"/>Alpi</a> , che circondano la regione ad ovest e nord, e gli Appennini che,
+						situati al confine con Liguria ed Emilia-Romagna, costituiscono cosi' un confine naturale.</P> 
+						<br>
+					<p>Piemonte significa ai piedi dei monti, cosi' definito perche' circondato su tre lati dalle montagne delle Alpi
+						Occidentali e dell'Appennino Ligure. La montagna piemontese ha un aspetto imponente ed aspro: infatti le
+						sommita' al di sopra dei tremila metri scendono rapidamente verso la pianura.</P>
+				<h2>Fiumi</h2>
+					<p>Il territorio piemontese e' ricco di corsi d'acqua, tutti tributari del fiume Po, che attraversa interamente la
+						regione da ovest a est. Da citare oltre allo stesso Po, sono:</P>
+				<nav>
+					<ul>
+						<li><strong>Tanaro</strong>, il principale affluente di destra, lungo 276 km con i suoi sub-affluenti principali, la Bormida e
+							la <a href="http://www.vallesturademonte.com"/>Stura di Demonte</a>;
+						<li><strong>Dora Riparia</strong>, affluente di sinistra, lungo 125 km, percorre interamente, coi suoi affluenti, la Valle di 
+							Susa;Dora Riparia, affluente di sinistra, lungo 125 km, percorre interamente, coi suoi affluenti, la Valle di
+							Susa;</li>
+						<li><strong>Sesia</strong>, lungo 140 km, attraversa il vercellese ed e' uno dei piu' importanti fiumi del Piemonte orientale;Sesia, lungo 140 km, attraversa il vercellese ed e' uno dei piu' importanti fiumi del Piemonte orientale;</li>
+						<li><strong>Ticino</strong>, lungo 248 km, il secondo fiume italiano per portata d'acqua, immissario ed emissario del <a href="lago-maggiore.html"/>Lago
+							Maggiore</a>, scorre pero' solo per pochi km interamente in Piemonte in quanto funge da confine con la
+							Lombardia;</p>
+					</ul>
+				</nav>
+				<h2>Le attivita'</h2>
+					<p>La maggior parte della popolazione vive in pianura in particolare a Torino e nei paesi limitrofi, dove si
+						concentra l'attivita' industriale. Nelle province di Vercelli e di Novara, l'economia e' basata sulla coltivazione
+						del riso di cui la regione e' il primo produttore italiano. Altri prodotti dell'agricoltura praticata con tecniche
+						macchinari moderni sono: cereali, patate, ortaggi, frutta e foraggio. Sulle colline sono diffusi i vigneti che
+						producono vini pregiati.</P>
+				<a href="novara.html"><img src="images/stemma-novara.jpg"/></a> <br><p>Clicca sullo stemma del Comune di Novara</P>
+				<h2>Clima</h2>
+					<p>Il Piemonte ha un clima tipicamente temperato a carattere sub-continentale che sulle Alpi diventa
+						progressivamente temperato-freddo e freddo salendo in quota. Nelle zone situate a bassa quota gli inverni
+						sono freddi ed umidi (spesso si formano nebbie fitte) ma poco piovosi. Le estati invece sono calde ed afose
+						con possibilita' di forti temporali, specialmente nelle zone a nord del Po.</p>
+						<br>
+					<p>Sulla piovosita' ha molta influenza la direzione di provenienza delle masse d'aria. Se esse sono umide e
+						provengono da sud, sud-est o est, la catena alpina sbarra loro la strada (si tratta del fenomeno detto stau): in
+						tal caso le precipitazioni possono anche essere molto abbondanti, specialmente sui primi versanti montani,
+						talvolta provocando alluvioni</P>
+				<script src="js/vendor/jquery.min.js"></script>
+				<script src="js/vendor/bootstrap.min.js."> </script>
+<?php
+
+	include_once 'footer.php';
+
+?>
